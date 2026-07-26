398cb4b7430d9bbe661a2dd673c84d2ab06c7167
Saquibhere08 <halimsaquib99@gmail.com>
2026-07-26 14:10:48 +0530

    Hashing Operations

---
+++ Hashing/HashingExample1.java
@@ -0,0 +1,24 @@
+/*
+Hashing: Hashing is a technique used to store and retrieve data quickly by converting
+a value (called a key) into an index using a hash function
+
+In simpler terms: Hashing is a way of finding data quickly by converting it into a unique location (index)
+ */
+
+package Hashing;
+
+import java.util.HashMap;
+
+public class HashingExample1 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> st=new HashMap<>();
+
+        st.put(101,"Alice");
+        st.put(102,"Saquib");
+
+        System.out.println(st.get(101));
+        System.out.println(st.get(102));
+
+        System.out.println(st.isEmpty());
+    }
+}
---
+++ Hashing/HashingExample2.java
@@ -0,0 +1,28 @@
+package Hashing;
+
+public class HashingExample2 {
+    public static void main(String[] args) {
+        hashOperations1();
+    }
+    //Hashing Operation 1
+    public static void hashOperations1(){
+
+    }
+    //Hashing Operation 2
+    public static void hashOperations2(){
+
+    }
+    //Hashing Operation 3
+    public static void hashOperations3(){
+
+    }
+}
+
+/*
+
+Hashing is the process of converting a key into an index for fast data storage and retrieval
+Operation	Average Time
+Insert	        O(1)
+Search	        O(1)
+Delete	        O(1)
+ */
