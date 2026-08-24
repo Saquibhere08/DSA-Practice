c9f2f94c74cc4c8c32a537495379be57f3cd28e0
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-08-24 22:04:26 +0530

    CollectionFramework

--- CoreJava/Part3/collectionsFramwork/set/TreeSetExample1.java
+++ CoreJava/Part3/collectionsFramwork/set/TreeSetExample1.java
@@ -1,18 +1,23 @@
 package CoreJava.Part3.collectionsFramwork.set;
 
-import java.util.Set;
 import java.util.TreeSet;
 
-public class TreeSetExample {
+public class TreeSetExample1 {
     public static void main(String[] args) {
-        Set<Integer>set=new TreeSet<>();
+        TreeSet<Integer> set=new TreeSet<>();
+
         set.add(10);
         set.add(21);
         set.add(32);
         set.add(11);
         set.add(28);
         set.add(30);
+
         System.out.println(set);
-        //Tree set will arranged the unsorted array into sorted array.
+
+        System.out.println(set.first());
+        System.out.println(set.last());
+        System.out.println(set.higher(20));
+        System.out.println(set.lower(10));
     }
 }
